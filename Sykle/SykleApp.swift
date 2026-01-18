7ef2ff2a0aaeb461768c2d4a24b78f612de35490
“Sanuzia <sanuziajorge@gmail.com>
2026-01-18 17:48:13 +0000

    Initial Commit

---
+++ Sykle/SykleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SykleApp.swift
+//  Sykle
+//
+//  Created by Sanuzia Jorge on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SykleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SykleTests/SykleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SykleTests.swift
+//  SykleTests
+//
+//  Created by Sanuzia Jorge on 18/01/2026.
+//
+
+import Testing
+@testable import Sykle
+
+struct SykleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
